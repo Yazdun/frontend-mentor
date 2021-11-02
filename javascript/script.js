63f95d91b58a34490c6a1b758a2f1b0d3e61323d
Yazdun <yazduntube@gmail.comm>
2021-11-02 15:18:24 +0330

    ⚙️Initial commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,24 @@
 // -----------------------------------------------------------------------------
 // This file contains all application-wide Javascripts.
 // -----------------------------------------------------------------------------
+
+const social = document.getElementById("social");
+const share = document.getElementById("share");
+
+window.addEventListener("click", (e) => {
+  try {
+    if (e.target.classList.contains("fa-share")) {
+      social.classList.toggle("active");
+    } else if (
+      e.target.classList.contains("social") ||
+      e.target.parentElement.classList.contains("social") ||
+      e.target.parentElement.parentElement.classList.contains("social")
+    ) {
+      return;
+    } else {
+      social.classList.remove("active");
+    }
+  } catch (error) {
+    social.classList.remove("active");
+  }
+});
