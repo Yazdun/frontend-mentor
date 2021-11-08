2fb8bdc0f39a8a42e70736ef86c8035f08c9c5d3
Yazdun <yazduntube@gmail.comm>
2021-11-08 11:16:15 +0330

    🔨 before adding intro animations

---
+++ javascript/modal.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById("btnMobile");
+
+const modal = btn.addEventListener("click", () => {
+  console.log("ok");
+});
+
+export default modal;
--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,29 @@
 // -----------------------------------------------------------------------------
 // This file contains all application-wide Javascripts.
 // -----------------------------------------------------------------------------
+
+var buttons = document.getElementsByClassName("btn");
+var alert = document.getElementById("alert");
+var closeAlert = document.getElementById("closeAlert");
+
+for (var i = 0; i < buttons.length; i++) {
+  console.log(buttons[i].classList);
+  buttons[i].addEventListener("click", () => {
+    alert.classList.remove("hide");
+    alert.classList.add("active");
+  });
+}
+
+closeAlert.addEventListener("click", () => {
+  alert.classList.remove("active");
+  alert.classList.add("hide");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target.classList.contains("btn")) {
+    // pass
+  } else {
+    alert.classList.remove("active");
+    alert.classList.add("hide");
+  }
+});
