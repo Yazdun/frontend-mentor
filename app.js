9b415f646cfe47051e97c61549d51b1b3002a4c3
Yazdun <yazduntube@gmail.com>
2022-02-20 10:25:41 +0330

    deployment : commit

--- app.js
+++ app.js
@@ -2,21 +2,28 @@ const btn = document.getElementById("btn");
 const adviceId = document.getElementById("adviceId");
 const adviceText = document.getElementById("adviceText");
 const card = document.getElementById("card");
+const errorText = document.getElementById("errorText");
 const loading = document.getElementById("loading");
 const url = "https://api.adviceslip.com/advice";
 
+const hideElement = (element) => (element.style.display = "none");
+const showElement = (element) => (element.style.display = "inline-block");
+
 async function getAdvice() {
   card.classList.add("hide");
-  loading.style.display = "inline-block";
+  showElement(loading);
   try {
     const res = await fetch(url);
     const data = await res.json();
     card.classList.remove("hide");
-    loading.style.display = "none";
+    hideElement(loading);
     adviceId.textContent = data.slip.id;
     adviceText.textContent = data.slip.advice;
   } catch (error) {
     console.log(error);
+    showElement(errorText);
+    hideElement(loading);
+    hideElement(card);
   }
 }
 
