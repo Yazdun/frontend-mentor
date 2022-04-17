bc0ce92dbb62637d0ad2f48796b130030936b432
Yazdun <yazduntube@gmail.com>
2022-04-17 18:04:26 +0430

    commit

--- components/navigation/index.jsx
+++ components/navigation/index.jsx
@@ -1,12 +1,15 @@
 import { ThemeSwitch } from 'components'
 import { Container } from 'elements'
+import Link from 'next/link'
 import s from './styles.module.scss'
 
 export const Navigation = () => {
   return (
     <nav className={s.nav}>
       <Container sx={s.wrap}>
-        <p className={s.title}>Where in the world?</p>
+        <Link href="/">
+          <a className={s.title}>Where in the world?</a>
+        </Link>
         <ThemeSwitch />
       </Container>
     </nav>
