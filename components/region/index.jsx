e2853da194f9b1e1b66bbd8935a1677a7a776fa2
Yazdun <yazduntube@gmail.com>
2022-04-18 15:14:09 +0430

    commit

--- components/region/index.jsx
+++ components/region/index.jsx
@@ -1,11 +1,37 @@
 import s from './styles.module.scss'
 import { BiChevronDown } from 'react-icons/bi'
+import { useState, useRef } from 'react'
+import { data } from './regions'
+import { motion } from 'framer-motion'
+import cn from 'classnames'
+import { useOnClickOutside } from 'hooks'
 
 export const Region = () => {
+  const [active, setActive] = useState(false)
+  const ref = useRef()
+  useOnClickOutside(ref, () => setActive(false))
+
   return (
-    <button className={s.select}>
-      Filter by Region
-      <BiChevronDown className={s.status} />
-    </button>
+    <div className={s.wrapper}>
+      <button className={s.select} onClick={() => setActive(prev => !prev)}>
+        Filter by Region
+        <BiChevronDown className={cn(s.status, active && s.rotate)} />
+      </button>
+
+      <motion.ul
+        animate={{ scale: active ? 1 : 0 }}
+        className={s.list}
+        ref={ref}
+      >
+        {data.map((i, index) => {
+          const { region } = i
+          return (
+            <li key={index}>
+              <button className={s.item}>{region}</button>
+            </li>
+          )
+        })}
+      </motion.ul>
+    </div>
   )
 }
---
+++ components/region/regions.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    region: 'africa',
+  },
+  {
+    region: 'america',
+  },
+  {
+    region: 'asia',
+  },
+  {
+    region: 'europe',
+  },
+  {
+    region: 'oceania',
+  },
+]
